e6a9d7bac7c92f921ebf331e662ccafc338338ef
Robert Haritonov <r@rhr.me>
2017-02-26 14:39:33 +0100

    add file confir via arg, fetch GH profile email first

--- index.js
+++ index.js
@@ -3,48 +3,53 @@ const async = require('async');
 const getEmailByGHName = require('./lib/getEmailByGHName.js');
 const getListOfUsers = require('./lib/getListOfUsers.js');
 const getGHUsername = require('./lib/getGHUsername.js');
+const args = process.argv.slice(2);
 
 const auth = {
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET
 };
 
-const run = async (csvPath, limit) => {
-    const users = await getListOfUsers(csvPath);
-    const limitUsers = limit ? users.slice(0, limit) : users;
-    const limitUsersLength = limitUsers.length;
+const run = async(csvPath, limit) => {
+  const users = await getListOfUsers(csvPath);
+  const limitUsers = limit ? users.slice(0, limit) : users;
+  const limitUsersLength = limitUsers.length;
 
-    async.mapLimit(limitUsers, 2, async (item, callback) => {
-        try {
-            const itemSplit = item.trim().split(' ');
+  async.mapLimit(limitUsers, 4, async(item, callback) => {
+    try {
+      const itemSplit = item.trim().split(' ');
 
-            if (itemSplit.length < 2) {
-                callback(null, '');
-                return;
-            }
+      if (itemSplit.length < 2) {
+        callback(null, '');
+        return;
+      }
 
-            const userName = await getGHUsername(item, auth);
+      const userName = await getGHUsername(item, auth);
 
-            if (userName) {
-                const email = await getEmailByGHName(userName, auth);
+      if (userName) {
+        const email = await getEmailByGHName(userName, auth);
 
-                callback(null, email);
+        callback(null, email);
 
-                console.log(`${item}, ${userName}, ${email}`);
+        console.log(`${item}, ${userName}, ${email}`);
 
-                return;
-            }
+        return;
+      }
 
-            callback(null, '');
-        } catch(error) {
-            callback(null, '');
-        }
-    }, (err, results) => {
-        const nonEmptyResults = results.filter(item => item !== '');
+      callback(null, '');
+    } catch (error) {
+      callback(null, '');
+    }
+  }, (err, results) => {
+    const nonEmptyResults = results.filter(item => item !== '');
 
-        console.log('results', nonEmptyResults);
-        console.log(`got ${limitUsersLength - nonEmptyResults.length} out of ${limitUsersLength}`);
-    });
+    console.log('results', JSON.stringify(nonEmptyResults, null, 2));
+    console.log(`got ${limitUsersLength - nonEmptyResults.length} out of ${limitUsersLength}`);
+  });
 };
 
-run('./users.csv');
+if (args && args[0]) {
+  run(args[0]);
+} else {
+  console.log('please specify CSV path as a first argument...');
+}
--- readme.md
+++ readme.md
@@ -4,6 +4,28 @@ Using GH search get the usernames and emails of people having only a list First 
 
 # Usage
 
-Create a CSV list called `./users.csv` with First and Last names (combined). Run `index.js` passing `CLIENT_ID` and `CLIENT_SECRET` envs for GH API auth.
+Prepare a CSV list called with First and Last names (combined). Run `index.js` passing `CLIENT_ID` and `CLIENT_SECRET` envs for GH API auth.
+
+```
+CLIENT_ID=xxx CLIENT_SECRET=xxx node index.js ./users.csv
+```
 
 Script can only execute 30 searches in a minute, following GH search API limitations.
+
+# TODO
+
+* Provide other config options
+* Add output methods
+
+# Input CVS file example
+
+```
+Adolfo Smith
+Adrian Williams
+```
+
+App will only take first column, so data may have more.
+
+```
+Adolfo Smith,something
+```
